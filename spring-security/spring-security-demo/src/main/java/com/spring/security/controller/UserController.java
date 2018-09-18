57a33009acc4d1cdcca8a054590a1b4f058f2c3a
sunxiaojun <1648171941@qq.com>
2018-09-18 22:32:03 +0800

    修正版本冲突问题
    修正版本冲突问题

---
+++ spring-security/spring-security-demo/src/main/java/com/spring/security/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.spring.security.controller;
+
+
+import com.spring.security.dto.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public List<User> query() {
+        List<User> users = new ArrayList<>();
+        users.add(new User());
+        users.add(new User());
+        users.add(new User());
+        return users;
+
+    }
+
+}
